23416762c527ef31c189840f10277fe4f61c0ae6
Toi Nguyen <thanhtoi123xyz@gmail.com>
2022-11-07 23:04:24 +0700

    fix port

--- server.js
+++ server.js
@@ -47,8 +47,7 @@ app.use('/api/admin', adminRouter);
 app.use('/api/warnings', warningRouter);
 
 const PORT = process.env.PORT || 5000;
-console.log(PORT);
 app.listen(PORT, () => console.log(`Server run in port ${PORT}`));
-// server.listen(5001, () => {
-//     console.log("Listening socket on port 5001");
-// })
+server.listen(5001, () => {
+    console.log("Listening socket on port 5001");
+})
